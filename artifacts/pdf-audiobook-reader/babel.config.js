761385ed813efe4e3d5ffd795eb1e410645d1ff9
Keiver Alvarez <134085280+DataDojok@users.noreply.github.com>
2026-09-03 16:22:59 -0400

    Refactor Babel config to use preset-env
    Updated Babel configuration to use '@babel/preset-env' and removed specific plugins.

--- artifacts/pdf-audiobook-reader/babel.config.js
+++ artifacts/pdf-audiobook-reader/babel.config.js
@@ -1,12 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // Force transforms on modern private class elements before Hermes sees them
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-      ['@babel/plugin-transform-class-properties', { loose: true }]
-    ]
+    presets: [
+      '@babel/preset-env',
+      'babel-preset-expo'
+    ],
+    plugins: []
   };
 };
