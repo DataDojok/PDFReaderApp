f961017f0ecacfe9b090e23e28a41eedea54358f
Keiver Alvarez <134085280+DataDojok@users.noreply.github.com>
2026-09-04 14:31:29 -0400

    Add lazyImports option to babel preset configuration

--- artifacts/pdf-audiobook-reader/babel.config.js
+++ artifacts/pdf-audiobook-reader/babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          unstable_transformImportMeta: true,
+          lazyImports: true
+        }
+      ]
+    ],
     plugins: [
       ['@babel/plugin-transform-class-static-block', { loose: true }],
       ['@babel/plugin-transform-class-properties', { loose: true }],
