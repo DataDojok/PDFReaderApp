96f97d9624687f811121689b84bc2ca429bbbf8d
Keiver Alvarez <134085280+DataDojok@users.noreply.github.com>
2026-09-04 14:21:43 -0400

    Update babel config to enable unstable transformImportMeta

--- artifacts/pdf-audiobook-reader/babel.config.js
+++ artifacts/pdf-audiobook-reader/babel.config.js
@@ -1,8 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
     plugins: [
+      ['@babel/plugin-transform-class-static-block'],
       ["@babel/plugin-transform-private-methods", { "loose": true }],
       ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
       ["@babel/plugin-transform-class-properties", { "loose": true }]
