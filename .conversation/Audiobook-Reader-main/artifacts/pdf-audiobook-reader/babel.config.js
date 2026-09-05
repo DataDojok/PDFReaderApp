5dbfceb19af4bee8a68fa1ce7449094e97c6a4f5
Keiver Alvarez <134085280+DataDojok@users.noreply.github.com>
2026-09-05 16:06:54 -0400

    Update Babel configuration with additional plugins

--- .conversation/Audiobook-Reader-main/artifacts/pdf-audiobook-reader/babel.config.js
+++ .conversation/Audiobook-Reader-main/artifacts/pdf-audiobook-reader/babel.config.js
@@ -1,7 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
-    plugins: ['@babel/plugin-transform-class-static-block'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          unstable_transformImportMeta: true,
+          lazyImports: false
+        }
+      ]
+    ],
+    plugins: [
+      '@babel/plugin-transform-class-static-block',
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-private-property-in-object'
+    ]
   };
 };
